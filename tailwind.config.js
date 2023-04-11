d26010b70f5fc38d52b350a45ed1d283dc848b97
Vladislav Kharchenko <har4enko.vladis@yandex.ru>
2023-04-11 16:36:18 +0400

    header prototype

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        blackFon: '#000000'
+      }
+    },
+  },
+  plugins: [],
+}
+
