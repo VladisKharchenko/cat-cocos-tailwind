7272a276ccd1df3fe775a96d0a774109d94726fe
Vladislav Kharchenko <har4enko.vladis@yandex.ru>
2023-04-14 15:31:42 +0400

    endHTML

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "3xl": "0px 0px 10px 10px rgba(0, 0, 0, 0.3)",
+      },
+    },
   },
   plugins: [],
 };
